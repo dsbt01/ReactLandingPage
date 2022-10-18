0141cf6fecff888c10d315e5b44633dcbf0160fb
Rivenshell <rivenshell111@gmail.com>
2022-10-17 21:08:36 -0400

    First Commit

---
+++ src/js/component/flexbox.jsx
@@ -0,0 +1,5 @@
+const Flexbox = (props) => {
+    return <h1>Flexbox</h1>
+} ;
+
+export default Flexbox;
---
+++ src/js/component/middle.jsx
@@ -0,0 +1,5 @@
+const Middle = (props) => {
+    return <h1>Middle</h1>
+} ;
+
+export default Middle;

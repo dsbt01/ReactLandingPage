989e245b9664e0ea3f10a341340baa5db6911339
David Suarez <dsuarez001@att.net>
2022-10-19 18:49:19 -0400

    bootstrap was added

--- src/js/component/cardComponent.jsx
+++ src/js/component/cardComponent.jsx
@@ -1,5 +1,6 @@
 //import react into the bundle
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CardComponent = () => {
     return <h1>Card Grid</h1>
--- src/js/component/singleCardComponent.jsx
+++ src/js/component/singleCardComponent.jsx
@@ -1,6 +1,6 @@
 //import react into the bundle
 import React from "react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const singleCardComponent = () => {
     return <h1>Single Card</h1>

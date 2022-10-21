5c046431a237da3c1afb214a3e097e1b4a14e2c4
Rodrigo Moraes <rodrigo.moraes001@mymdc.net>
2022-10-21 22:51:43 +0000

    paRTIAl save

--- src/js/component/cardComponent.jsx
+++ src/js/component/cardComponent.jsx
@@ -1,60 +1,14 @@
 //import react into the bundle
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import SingleCardComponent from "./singleCardComponent.jsx";
 
 const CardComponent = () => {
-    return(
-<div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-      
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
+  return (
+    <div className="row mt-3">
+      <SingleCardComponent text="card1" />
     </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-</div>
-    );
-} ;
+  );
+};
 
 export default CardComponent;
--- src/js/component/singleCardComponent.jsx
+++ src/js/component/singleCardComponent.jsx
@@ -1,9 +1,20 @@
 //import react into the bundle
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const singleCardComponent = () => {
-    return <h1>Single Card</h1>
-} ;
+const singleCardComponent = (props) => {
+  return (
+    <div className="card col-3" >
+      <img src="" className="card-img-top" alt="" />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">{props.text}</p>
+        <a href="#" className="btn btn-primary">
+          Find out more!
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default SingleCardComponent;
